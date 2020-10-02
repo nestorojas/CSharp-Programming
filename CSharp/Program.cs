41e0dc2822375eed4a95233c21d7c1b605f94fe1
Nestor Rojas <nestor.rojas@live.com>
2020-10-02 13:48:45 -0400

    Init

---
+++ CSharp/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace CSharp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            FloorTemperature(0);
+        }
+        public static void FloorTemperature(float degrees)
+        {
+            object degreesRef = degrees;
+            var result = (int)(float)degreesRef;
+            Console.WriteLine(result);
+        }
+    }
+}
